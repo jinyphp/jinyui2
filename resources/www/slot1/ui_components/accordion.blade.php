40e30b54bddfc04d8f8cbd7976dbd4e3e591ad37
kkk1k <googija9818@gmail.com>
2024-06-28 17:18:05 +0900

    ui_components 폴더 생생 및 custuom 진행

---
+++ resources/www/slot1/ui_components/accordion.blade.php
@@ -0,0 +1,8 @@
+<x-www-layout>
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_accordion_code1"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_accordion_code1"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_accordion_code1"])
+</x-www-layout>
