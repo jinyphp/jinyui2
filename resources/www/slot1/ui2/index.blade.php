d6d128649bae65219f11e226aa5e63f2bb2ef859
kkk1k <googija9818@gmail.com>
2024-06-28 15:56:40 +0900

    widget 파일 변경

---
+++ resources/www/slot1/ui2/index.blade.php
@@ -0,0 +1,9 @@
+<x-www-layout>
+    <section class="py-5">
+        <div class="container px-5">
+            Hello
+            @livewire('WidgetCode-Preview', [
+            'filename' => "component_alert_code1"])
+        </div>
+    </section>
+</x-www-layout>
