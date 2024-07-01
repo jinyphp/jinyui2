b011d79e2a11c7c15c17908c676addaf1df7ae42
bosung si <qweasdzxc0311@naver.com>
2024-07-01 21:30:48 +0900

    기본 컴포넌트 생성 및 필요한 위젯 생성

--- resources/www/slot1/ui_components/accordion.blade.php
+++ resources/www/slot1/ui_components/accordion.blade.php
@@ -1,8 +1,11 @@
 <x-www-layout>
     @livewire('WidgetCode-Component', [
-    'filename' => "component_accordion_code1"])
+    'filename' => "component_accordion_code1"
+])
     @livewire('WidgetCode-Component', [
-    'filename' => "component_accordion_code1"])
+    'filename' => "component_accordion_code1"
+])
     @livewire('WidgetCode-Component', [
-    'filename' => "component_accordion_code1"])
-</x-www-layout>
+    'filename' => "component_accordion_code1"
+])
+</x-www-layout>b
---
+++ resources/www/slot1/ui_components/tooltips.blade.php
@@ -0,0 +1,41 @@
+<x-www-layout>
+    @livewire('WidgetTitle', [
+    'filename' => "component_tooltips_title"
+])
+    @livewire('WidgetCode-OnlyCode', [
+    'filename' => "component_tooltips_Enable"
+])
+    @livewire('WidgetCode-OnlyCode', [
+    'filename' => "component_tooltips_Custom"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_tooltips_Custom2"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_tooltips_TooltipsOnLinks"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_tooltips_Directions"
+])
+    @livewire('WidgetCode-OnlyCode', [
+    'filename' => "component_tooltips_Directions2"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_tooltips_Usage"
+])
+<h2>CSS</h2>
+    @livewire('WidgetCode-OnlyCode', [
+    'filename' => "component_tooltips_Variables"
+])
+    @livewire('WidgetCode-OnlyCode', [
+    'filename' => "component_tooltips_Sass"
+])
+    @livewire('WidgetCode-OnlyCode', [
+    'filename' => "component_tooltips_Usage"
+])
+    @livewire('WidgetCode-OnlyCode', [
+    'filename' => "component_tooltips_Markup"
+])
+
+
+</x-www-layout>
