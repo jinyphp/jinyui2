c4a92fcef04757e7bac55ad864b22f013838871b
qzzloz <xlivsxd@gmail.com>
2024-07-04 16:56:08 +0900

    부트스트랩 accordion 시작

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css2/scss/app-saas.scss',
                 'resources/css/app.scss',
                 'resources/css/tailwind.scss',
                 'resources/js/app.js'
