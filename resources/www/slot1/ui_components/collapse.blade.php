11118f1d962a8eb96f6c758fcb7f5e42d3713090
kkk1k <googija9818@gmail.com>
2024-07-04 14:45:45 +0900

    widget 작업 완료

---
+++ resources/www/slot1/ui_components/collapse.blade.php
@@ -0,0 +1,26 @@
+<x-www-layout>
+    @livewire('WidgetTitle', [
+    'filename' => "component_collapse_code1"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_collapse_code2"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_collapse_code3"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_collapse_code4"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_collapse_code5"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_collapse_code6"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_collapse_code7"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_collapse_code8"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_collapse_code9"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_collapse_code10"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_collapse_code11"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_collapse_code12"])
+</x-www-layout>
---
+++ resources/www/slot1/ui_components/dropdown.blade.php
@@ -0,0 +1,96 @@
+<x-www-layout>
+    @livewire('WidgetTitle', [
+    'filename' => "component_dropdown_code1"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code2"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code3"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code4"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code5"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code6"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code7"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code8"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code9"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code10"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code11"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code12"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code13"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code14"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code15"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code16"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code17"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code18"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code19"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code20"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code21"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code22"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code23"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code24"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code25"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code26"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code27"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code28"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code29"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code30"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code31"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code32"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code33"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code34"])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_dropdown_code35"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code36"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code37"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code38"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code39"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code40"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code41"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code42"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code43"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code44"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code45"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code46"])
+    @livewire('WidgetOnlyCode', [
+    'filename' => "component_dropdown_code47"])
+</x-www-layout>
