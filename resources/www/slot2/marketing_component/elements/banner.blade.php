37857d33618fa7d69a8d10967af11df0b78c1100
bosung si <qweasdzxc0311@naver.com>
2024-08-05 23:54:10 +0900

    elements, feedback 추가

---
+++ resources/www/slot2/marketing_component/elements/banner.blade.php
@@ -0,0 +1,44 @@
+<x-www-layout>
+    @livewire('WidgetTitle', [
+    'filename' => "component_banner"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_With_button"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_On_dark"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_On_brand"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_With_background_glow"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_With_link"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_Left-aligned"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_Bottom_aligned"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_Floating_at_bottom"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_Floating_at_bottom_centered"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_Privacy_notice_right-aligned"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_Privacy_notice_centered"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_Privacy_notice_left-aligned"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_banner_Privacy_notice_full_width"
+])
+</x-www-layout>
---
+++ resources/www/slot2/marketing_component/elements/header.blade.php
@@ -0,0 +1,41 @@
+<x-www-layout>
+    @livewire('WidgetTitle', [
+    'filename' => "component_header"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_With_stacked_flyout_menu"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_Constrained"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_On_dark_background"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_On_brand_background"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_With_full_width_flyout_menu"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_Full_width"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_With_call-to-action"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_With_multiple_flyout_menus"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_With_icons_in_mobile_menu"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_With_left-aligned_nav"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_With_right-aligned_nav"
+])
+    @livewire('WidgetCode-Component', [
+    'filename' => "component_header_With_centered_logo"
+])
+</x-www-layout>
