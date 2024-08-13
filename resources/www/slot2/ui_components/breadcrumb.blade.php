94b605dab2c25b4a564c879713f232d97d2a1fc5
kkk1k <googija9818@gmail.com>
2024-08-13 16:31:13 +0900

    note .blade.php 추가

--- resources/www/slot2/ui_components/breadcrumb.blade.php
+++ resources/www/slot2/ui_components/breadcrumb.blade.php
@@ -7,9 +7,8 @@
     'filename' => "component_breadcrumb_code3"])
     @livewire('WidgetOnlyCode', [
     'filename' => "component_breadcrumb_code5"])
-    <p>인라인 SVG에는 올바르게 이스케이프된 문자가 필요합니다. <,> 및 #와 같은 일부 예약 문자는 URL 인코딩 또는 이스케이프되어야 합니다. 이 작업은 escape-svg() Sass 함수를 사용하여
-            $breadcrumb-divider 변수를 통해 수행합니다. CSS 변수를 사용자 정의할 때는 직접 처리해야 합니다. 자세한 내용은 CodePen에서의 Kevin Weber의 설명을 참조하세요.
-    </p>
+    @livewire('WidgetNote', [
+    'filename' => "component_breadcrumb_accesibility"])
     @livewire('WidgetCode-Component', [
     'filename' => "component_breadcrumb_code4"])
     @livewire('WidgetOnlyCode', [
