6a6d3727b22a1b7b34f19067a9dde8315c2340fd
kkk1k <googija9818@gmail.com>
2024-08-18 18:44:39 +0900

    footer 생성 및 header link 수정

--- resources/www/docs1/_partials/footer_menu.blade.php
+++ resources/www/docs1/_partials/footer_menu.blade.php
@@ -1,4 +1,4 @@
-<div class="accordion" id="footerLinks">
+<!-- <div class="accordion" id="footerLinks">
     <div class="row row-cols-1 row-cols-sm-3">
         <div class="accordion-item col border-0">
             <h6 class="accordion-header" id="categoriesHeading">
@@ -7,107 +7,49 @@
                     data-bs-target="#categoriesLinks" aria-expanded="false"
                     aria-controls="categoriesLinks">Categories</button>
             </h6>
-            <div class="accordion-collapse collapse d-sm-block" id="categoriesLinks" aria-labelledby="categoriesHeading"
+            <div class="flex justify-between" id="categoriesLinks" aria-labelledby="categoriesHeading"
                 data-bs-parent="#footerLinks">
-                <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
+                <ul class="nav  gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
                     <li class="d-flex w-100 pt-1">
                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">For women</a>
+                            href="#!">Docs</a>
                     </li>
                     <li class="d-flex w-100 pt-1">
                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">For men</a>
+                            href="#!">Utility</a>
                     </li>
                     <li class="d-flex w-100 pt-1">
                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Home clothes</a>
+                            href="#!">Blade Components</a>
                     </li>
                     <li class="d-flex w-100 pt-1">
                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Accessories</a>
+                            href="#!">Block Widgets</a>
                     </li>
                     <li class="d-flex w-100 pt-1">
                         <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Sale</a>
-                    </li>
-                </ul>
-            </div>
-            <hr class="d-sm-none my-0">
-        </div>
-        <div class="accordion-item col border-0">
-            <h6 class="accordion-header" id="accountHeading">
-                <span class="text-dark-emphasis d-none d-sm-block">Account</span>
-                <button type="button" class="accordion-button collapsed py-3 d-sm-none" data-bs-toggle="collapse"
-                    data-bs-target="#accountLinks" aria-expanded="false" aria-controls="accountLinks">Account</button>
-            </h6>
-            <div class="accordion-collapse collapse d-sm-block" id="accountLinks" aria-labelledby="accountHeading"
-                data-bs-parent="#footerLinks">
-                <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Your account</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Shipping &amp; policies</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Refunds &amp; replacements</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Order tracking</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Delivery info</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Taxes &amp; fees</a>
-                    </li>
-                </ul>
-            </div>
-            <hr class="d-sm-none my-0">
-        </div>
-        <div class="accordion-item col border-0">
-            <h6 class="accordion-header" id="customerHeading">
-                <span class="text-dark-emphasis d-none d-sm-block">Customer service</span>
-                <button type="button" class="accordion-button collapsed py-3 d-sm-none" data-bs-toggle="collapse"
-                    data-bs-target="#customerLinks" aria-expanded="false" aria-controls="customerLinks">Customer
-                    service</button>
-            </h6>
-            <div class="accordion-collapse collapse d-sm-block" id="customerLinks" aria-labelledby="customerHeading"
-                data-bs-parent="#footerLinks">
-                <ul class="nav flex-column gap-2 pt-sm-3 pb-3 pb-sm-0 mt-n1 mb-1 mb-sm-0">
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Payment methods</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Money back guarantee</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Refunds &amp; replacements</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Product returns</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Support center</a>
-                    </li>
-                    <li class="d-flex w-100 pt-1">
-                        <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0"
-                            href="#!">Shipping</a>
+                            href="#!">Icons</a>
                     </li>
                 </ul>
             </div>
             <hr class="d-sm-none my-0">
         </div>
     </div>
-</div>
+</div>-->
+<ul class="flex flex-row justify-between w-100 list-none no-underline p-0 text-center">
+    <li class="">
+        <a class=" no-underline text-black " href="/tailwind/backgrounds/backgroundAttachment">Docs</a>
+    </li>
+    <li class=" ">
+        <a class="no-underline text-black" href="/tailwind/backgrounds/backgroundAttachment">Utility</a>
+    </li>
+    <li class=" ">
+        <a class="no-underline text-black" href="/ui_components/accordion">Blade Components</a>
+    </li>
+    <li class=" ">
+        <a class="no-underline text-black" href="/blocks/hero">Block Widgets</a>
+    </li>
+    <li class=" ">
+        <a class="no-underline text-black" href="#!">Icons</a>
+    </li>
+</ul>
